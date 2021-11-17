7ac1b36c5e80dde2cd2f447ca4c584fb80349794
yoyo <595460399@qq.com>
2021-11-17 12:47:20 +0800

    pc-header done

---
+++ src/components/head/components/pc-head.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="headBox">
+    <el-menu mode="horizontal">
+      <el-menu-item index="/">
+        <i class="fa fa-wa fa-home" /> 首页
+      </el-menu-item>
+      <el-submenu index="">
+        <template slot="title">
+          <i class="fa fa-wa fa-flask" /> 实验室
+        </template>
+      </el-submenu>
+      <el-menu-item index="/archive">
+        <i class="fa fa-wa fa-archive" /> 归档
+      </el-menu-item>
+      <el-menu-item index="/reward">
+        <i class="fa fa-wa fa-cny" /> 赞赏
+      </el-menu-item>
+      <el-menu-item index="/friendslink">
+        <i class="fa fa-wa fa-users" /> 伙伴
+      </el-menu-item>
+      <el-menu-item index="/message">
+        <i class="fa fa-wa fa-pencil" /> 留言板
+      </el-menu-item>
+      <el-menu-item index="/aboutme">
+        <i class="fa fa-wa fa-vcard" /> 关于
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PCHead'
+}
+</script>
+
+<style lang="less">
+.headBox {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 100;
+
+  width: 100%;
+  height: 38px;
+  background: transparent;
+  background-color: #48456c;
+}
+.headBox .el-menu {
+  height: 38px;
+  max-width: 80%;
+  margin: 0 auto;
+  border-bottom: none !important;
+  background: transparent;
+}
+.headBox .el-menu > .el-menu-item,
+.headBox .el-menu > .el-submenu .el-submenu__title {
+  height: 38px;
+  line-height: 38px;
+  border-bottom: none !important;
+
+  color: #fff;
+}
+
+// .headBox .el-menu > .el-menu-item:hover,
+// .headBox .el-menu > .el-submenu .el-submenu__title:hover {
+//   background-color: #48456c;
+// }
+
+.headBox .el-menu > .el-menu-item i,
+.headBox .el-menu > .el-submenu .el-submenu__title i {
+  color: #fff;
+}
+</style>
---
+++ src/components/head/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="headBack">
+      <el-row class="container">
+        <el-col :span="24">
+          <PCHead class="pc-head"></PCHead>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="headImgBox"></div>
+  </div>
+</template>
+
+<script>
+import PCHead from './components/pc-head.vue'
+export default {
+  name: 'Head',
+  components: {
+    PCHead
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
